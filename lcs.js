d5e3b67c18f646e9d7bdc574322805825712621a
Aliaksandr Kliamita <alexander.klemyato@gmail.com>
2025-09-27 17:14:39 +0300

    sixth commit

--- lcs.js
+++ lcs.js
@@ -16,18 +16,17 @@ const lcm = (a, b) => {
   const x = +(url.searchParams.get('x'));
   const y = +(url.searchParams.get('y'));
 
-  if (url.pathname !== `/app/${email}`  && url.pathname !== `/app/${email}/`) {
+  if (url.pathname !== `/app/${email}`) {
       return res.end("NaN"); 
   }
     
-    // || x > 1e9 || y > 1e9
-  if (!Number.isInteger(x) || !Number.isInteger(y) || x <= 0 || y <= 0 ) { 
+  if (!Number.isInteger(x) || !Number.isInteger(y) || x <= 0 || y <= 0 || x > 1e9 || y > 1e9) { 
     return res.end('NaN')
   }
 
 
   res.end((lcm(x,y).toString()));
-}).listen(PORT, '0.0.0.0', () => { 
+}).listen(PORT, () => { 
   console.log(`http://localhost:${PORT}/app/${email}?x={}&y={}`);
 });
 
